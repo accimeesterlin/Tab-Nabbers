8465fdc390b57437d49e676e5d7e1a6d4da63468
Ashley MacWhirter <macwhirtera@gmail.com>
2017-05-11 19:04:42 -0400

    saving changes

--- server.js
+++ server.js
@@ -33,18 +33,18 @@ var authRoute = require('./app/routes/auth.js')(app, passport);
 
 //load passport strategies
 require('./app/config/passport/passport.js')(passport, models.user);
- 
+
 //Sync Database
 models.sequelize.sync().then(function() {
     console.log('Nice! Database looks fine');
 }).catch(function(err) {
     console.log(err, "Something went wrong with the Database Update!");
 });
- 
+
 app.listen(port, function(err) {
- 
+
     if (!err)
         console.log("Site is live");
-    else console.log(err)
- 
+    else console.log(err);
+
 });
