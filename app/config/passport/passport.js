045067118bbeaa5f058a50341bcaf31d7bb034fa
Eric Gaupp <ericgaupp@Kimberlys-MacBook-Air.local>
2017-05-12 19:57:46 -0400

    Passport

--- app/config/passport/passport.js
+++ app/config/passport/passport.js
@@ -21,29 +21,20 @@ module.exports = function(passport, user) {
         });
     });
 
-<<<<<<< HEAD
-    passport.use('local-signup', new LocalStrategy({
-=======
 
 
-////////////////////////////////////////////////////////////////////
-///////////////////Recruiter Access Below///////////////////////////
-////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////
+    ///////////////////Recruiter Access Below///////////////////////////
+    ////////////////////////////////////////////////////////////////////
     // Sign Up Recruiter
-    passport.use('recruiter-signup', new LocalStrategy(
-        {
->>>>>>> a86ea5f6b02fc52ed893593f244021eb0a7ab1f6
+    passport.use('recruiter-signup', new LocalStrategy({
             usernameField: 'email',
             passwordField: 'password',
             passReqToCallback: true // allows us to pass back the entire request to the callback
         },
 
-<<<<<<< HEAD
         function(req, email, password, done) {
-=======
-        function (req, email, password, done) {
             // Creating long string password for users
->>>>>>> a86ea5f6b02fc52ed893593f244021eb0a7ab1f6
             var generateHash = function(password) {
                 return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
             };
@@ -59,14 +50,9 @@ module.exports = function(passport, user) {
                     });
 
                 } else {
-                    console.log('Creating new user');
                     var userPassword = generateHash(password);
 
-<<<<<<< HEAD
-                    var student = {
-=======
                     var recruiter = {
->>>>>>> a86ea5f6b02fc52ed893593f244021eb0a7ab1f6
                         email: email,
                         password: userPassword,
                         firstname: req.body.firstname,
@@ -89,8 +75,7 @@ module.exports = function(passport, user) {
         }
     ));
     //LOCAL SIGNIN
-<<<<<<< HEAD
-    passport.use('local-signin', new LocalStrategy({
+    passport.use('recruiter-signin', new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
             usernameField: 'email',
             passwordField: 'password',
@@ -98,43 +83,21 @@ module.exports = function(passport, user) {
         },
 
         function(req, email, password, done) {
-            var User = user;
-            var isValidPassword = function(userpass, password) {
-                return bCrypt.compareSync(password, userpass);
-            }
-
-            User.findOne({
-                where: {
-                    email: email
-                }
-            }).then(function(user) {
-=======
-    passport.use('recruiter-signin', new LocalStrategy(
-        {
-            // by default, local strategy uses username and password, we will override with email
-            usernameField : 'email',
-            passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
-        },
-
-        function(req, email, password, done) {
             // Generating long string password
-            var isValidPassword = function(userpass,password){
+            var isValidPassword = function(userpass, password) {
                 return bCrypt.compareSync(password, userpass);
             };
 
             Recruiter.findOne({
-                where : {
+                where: {
                     email: email
                 }
-            }).then(function (user) {
->>>>>>> a86ea5f6b02fc52ed893593f244021eb0a7ab1f6
+            }).then(function(user) {
 
                 if (!user) {
                     return done(null, false, { message: 'Email does not exist' });
                 }
 
-<<<<<<< HEAD
                 if (!isValidPassword(user.password, password)) {
 
                     return done(null, false, { message: 'Incorrect password.' });
@@ -153,30 +116,4 @@ module.exports = function(passport, user) {
             });
         }
     ));
-
-}
-=======
-                if (!isValidPassword(user.password,password)) {
-
-                    return done(null, false, { message: 'Incorrect password.' });
-
-                }
-
-                var userinfo = user.get();
-
-                return done(null,userinfo);
-
-            }).catch(function(err){
-
-                console.log("Error:",err);
-
-                return done(null, false, { message: 'Something went wrong with your Signin' });
-            });
-        }
-    )); // Sigin Recruiter
-
-
-
 };
-
->>>>>>> a86ea5f6b02fc52ed893593f244021eb0a7ab1f6
