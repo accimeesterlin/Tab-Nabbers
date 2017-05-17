dbced69f7b32b9f1a66631969f23ead14f19a245
Eric Gaupp <ericgaupp@Kimberlys-MacBook-Air.local>
2017-05-17 01:02:14 -0400

    Bug Fix for Dynamically Added Form data

--- app/public/js/signup.js
+++ app/public/js/signup.js
@@ -1,43 +1,41 @@
 $(document).ready(function() {
 
     var url = 'http://localhost:8080/api/bootcamps';
+    var initialBootcampID;
 
     $.ajax({
         method: 'GET',
         url: url
     }).done(function(result) {
-        var initialBootcampID;
-        //Loop thru the results
+        console.log(result);
         for (var i = 0; i < result.length; i++) {
-            if (i = 0) {
+            if (i === 0) {
                 initialBootcampID = result[i].id;
             }
             //Create each option
+            var id = result[i].id;
             var institution = result[i].institution;
             $('#bootcamp').append($('<option>', {
                 value: result[i].id,
-                text: result[i].institution
+                text: institution
             }));
+
         }
 
         //Populate Cohorts
-        // $.ajax({
-            //     method: 'GET',
-            //     url: url + '/' + initialBootcampID
-            // }).done(function(cohortResults) {
-            //     for (var i = 0; i < cohortResults.length; i++) {
-            //         var cohort = cohortResults[i].cohort;
-            //         $('#cohort').append($('<option>', {
-            //             value: cohortResults[i].id,
-            //             text: cohortResults[i].cohort
-            //         }));
-            //     }
-            // }).fail(function(err) {
-            //     console.log(err);
-            // });
-
-    }).fail(function(err) {
-        console.log(err);
+        $.ajax({
+            method: 'GET',
+            url: url + '/' + initialBootcampID
+        }).done(function(cohortResults) {
+            console.log(cohortResults);
+            for (var i = 0; i < cohortResults.length; i++) {
+                var cohort = cohortResults[i].cohort;
+                $('#cohort').append($('<option>', {
+                    value: cohortResults[i].id,
+                    text: cohortResults[i].cohort
+                }));
+            }
+        });
     });
 
     $('#bootcamp').change(function() {
@@ -46,7 +44,6 @@ $(document).ready(function() {
             method: 'GET',
             url: url + '/' + bootcampID
         }).done(function(cohortResults) {
-            console.log(cohortResults);
             $('#cohort').empty();
             for (var i = 0; i < cohortResults.length; i++) {
                 var cohort = cohortResults[i].cohort;
