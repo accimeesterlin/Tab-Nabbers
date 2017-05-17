1551cf3c57250d938f7a38b527b05689a29659ec
Ashley MacWhirter <macwhirtera@gmail.com>
2017-05-16 21:27:42 -0400

    coordinates update

--- app/public/js/california.js
+++ app/public/js/california.js
@@ -8,27 +8,27 @@ var w = 1200,
     x_browser = 20,
     y_browser = 25,
     root;
- 
+
 var caliVis;
-var force = d3.layout.force(); 
+var force = d3.layout.force();
 
 caliVis = d3.select("#caliVis").append("svg").attr("width", w).attr("height", h);
- 
+
 d3.json("california.json", function(json) {
   root = json;
   root.fixed = true;
   root.x = w / 2;
   root.y = h / 4;
- 
- 
+
+
         // Build the path
   var defs = caliVis.insert("svg:defs")
       .data(["end"]);
- 
- 
+
+
   defs.enter().append("svg:path")
       .attr("d", "M0,-5L10,0L0,5");
- 
+
      update();
 
    function collapse(d) {
@@ -46,14 +46,14 @@ d3.json("california.json", function(json) {
    collapseAll();
 });
 
- 
+
 /**
- *   
+ *
  */
 function update() {
   var nodes = flatten(root),
       links = d3.layout.tree().links(nodes);
- 
+
   // Restart the force layout.
   force.nodes(nodes)
         .links(links)
@@ -65,38 +65,38 @@ function update() {
         .size([w, h])
         .on("tick", tick)
         .start();
- 
+
    var path = caliVis.selectAll("path.link")
       .data(links, function(d) { return d.target.id; });
- 
+
     path.enter().insert("svg:path")
       .attr("class", "link")
       // .attr("marker-end", "url(#end)")
       .style("stroke", "#eee");
- 
- 
+
+
   // Exit any old paths.
   path.exit().remove();
 
- 
+
   // Update the nodes…
   var node = caliVis.selectAll("g.node")
       .data(nodes, function(d) { return d.id; });
- 
- 
+
+
   // Enter any new nodes.
   var nodeEnter = node.enter().append("svg:g")
       .attr("class", "node")
       .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
       .on("click", click);
     //   .call(force.drag);
- 
+
   // Append a circle
   nodeEnter.append("svg:circle")
       .attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; })
       .style("fill", "#eee");
- 
-   
+
+
   // Append images
   var images = nodeEnter.append("svg:image")
         .attr("xlink:href",  function(d) { return d.img;})
@@ -104,14 +104,14 @@ function update() {
         .attr("y", function(d) { return -25;})
         .attr("height", 50)
         .attr("width", 50);
-  
+
   // make the image grow a little on mouse over and add the text details on click
   var setEvents = images
           // Append hero text
           .on( 'click', function (d) {
-              d3.select("h1").html(d.hero); 
-              d3.select("h2").html(d.name); 
-              d3.select("h3").html ("Take me to " + "<a href='" + d.link + "' >"  + d.hero + " web page ⇢"+ "</a>" ); 
+              d3.select("h1").html(d.hero);
+              d3.select("h2").html(d.name);
+              d3.select("h3").html ("Take me to " + "<a href='" + d.link + "' >"  + d.hero + " web page ⇢"+ "</a>" );
            })
 
           .on( 'mouseenter', function() {
@@ -140,49 +140,49 @@ function update() {
       .attr("y", y_browser +15)
       .attr("fill", tcBlack)
       .text(function(d) { return d.hero; });
- 
- 
+
+
   // Exit any old nodes.
   node.exit().remove();
- 
- 
+
+
   // Re-select for update.
   path = caliVis.selectAll("path.link");
   node = caliVis.selectAll("g.node");
- 
+
 function tick() {
- 
- 
+
+
     path.attr("d", function(d) {
- 
+
      var dx = d.target.x - d.source.x,
            dy = d.target.y - d.source.y,
            dr = Math.sqrt(dx * dx + dy * dy);
-           return   "M" + d.source.x + "," 
-            + d.source.y 
-            + "A" + dr + "," 
-            + dr + " 0 0,1 " 
-            + d.target.x + "," 
+           return   "M" + d.source.x + ","
+            + d.source.y
+            + "A" + dr + ","
+            + dr + " 0 0,1 "
+            + d.target.x + ","
             + d.target.y;
   });
-    node.attr("transform", nodeTransform);    
+    node.attr("transform", nodeTransform);
   }
 }
 
- 
+
 /**
  * Gives the coordinates of the border for keeping the nodes inside a frame
  * http://bl.ocks.org/mbostock/1129492
- */ 
+ */
 function nodeTransform(d) {
   d.x =  Math.max(maxNodeSize, Math.min(w - (d.imgwidth/2 || 16), d.x));
     d.y =  Math.max(maxNodeSize, Math.min(h - (d.imgheight/2 || 16), d.y));
     return "translate(" + d.x + "," + d.y + ")";
    }
- 
+
 /**
  * Toggle children on click.
- */ 
+ */
 function click(d) {
   if (d.children) {
     d._children = d.children;
@@ -193,23 +193,23 @@ function click(d) {
   }
   update();
 }
- 
- 
+
+
 /**
  * Returns a list of all nodes under the root.
- */ 
+ */
 function flatten(root) {
-  var nodes = []; 
+  var nodes = [];
   var i = 0;
- 
+
   function recurse(node) {
-    if (node.children) 
+    if (node.children)
       node.children.forEach(recurse);
-    if (!node.id) 
+    if (!node.id)
       node.id = ++i;
     nodes.push(node);
   }
- 
+
   recurse(root);
   return nodes;
 }
